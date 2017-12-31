0688b8c4bab74fd7f8d064b0f7450580274cc65b
noneedsystem <noneedsystem@gmail.com>
2017-12-31 09:26:23 +0000

    now exchange & single coin can be scraped

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cheerio   = require('cheerio');
 const URL = 'https://coinmarketcap.com';
 const SELECTOR_COINS    = 'table#currencies tbody tr';
 const SELECTOR_MARKETS  = '#markets-table tbody tr';
+const SELECTOR_EXCHANGE = 'div#markets table tbody tr'
 
 const coinMarks = (slc = -1) => 
   fetch(URL)
@@ -19,6 +20,25 @@ const coinMarks = (slc = -1) =>
     })))
     
 module.exports = coinMarks;
+
+module.exports.single = (coinLink) => 
+  fetch(URL + coinLink)
+    .then(res => res.text())
+    .then(html => cheerio.load(html))
+    .then($ => scrapSingle($))
+    
+module.exports.exchange = (exchangeLink) =>
+  fetch(URL + exchangeLink)
+    .then(res => res.text())
+    .then(html => cheerio.load(html))
+    .then($ => $(SELECTOR_EXCHANGE).map((i, el) => scrapExchange($(el))).get())
+    .then(arr => arr.filter((pair, index) => pair.name && index === arr.findIndex(p => p.name === pair.name)))
+    .then(coins => Promise.all(coins.map(coin => {
+      return fetch(URL + coin.link)
+        .then(res => res.text())
+        .then(html => cheerio.load(html))
+        .then($ => scrapSingle($, coin))
+    })))
     
     
   function scrapCoin(el){
@@ -40,6 +60,49 @@ module.exports = coinMarks;
         usd: elems.cap.attr('data-usd'),
         btc: elems.cap.attr('data-btc')
       },
+      price: {
+        usd: elems.price.attr('data-usd'),
+        btc: elems.price.attr('data-btc')
+      }
+    }
+  }
+  
+  function scrapSingle($){
+    const elems = {
+      symbol: $('h1.text-large small.bold.hidden-xs'),
+      // link:   $('a.currency-name-container'),
+      rank:   $('span.glyphicon.glyphicon-star').next(),
+      price:  $('span#quote_price'),
+      cap:    $('span[data-currency-market-cap]'),
+      logo:   $('img.currency-logo-32x32')
+    };
+    
+    return scrapExtras($, {
+      symbol: elems.symbol.text().replace('(', '').replace(')', ''),
+      name:   elems.logo.attr('alt'),
+      rank:   elems.rank.text().replace(' Rank ', ''),
+      // link:   elems.link.attr('href'),
+      logo:   elems.logo.attr('src'),
+      cap: {
+        usd: elems.cap.attr('data-usd'),
+        //btc: elems.cap.attr('data-btc')
+      },
+      price: {
+        usd: elems.price.attr('data-usd'),
+        //btc: elems.price.attr('data-btc')
+      }
+    })
+  }
+  
+  function scrapExchange(el){
+    const elems = {
+      link: el.find('td a.market-name'),
+      price: el.find('td span.price'),
+    }
+    
+    return {
+      name: elems.link.text(),
+      link: elems.link.attr('href'),
       price: {
         usd: elems.price.attr('data-usd'),
         btc: elems.price.attr('data-btc')
--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-require('./index.js')(-5).then(console.log);
+//require('./index.js')(-5).then(console.log);
+//require('./index.js').single('/currencies/experience-points/').then(console.log);
+require('./index.js').exchange('/exchanges/coinexchange/').then(console.log);
