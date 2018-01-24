74c821d000ce214ec2efa8220a52bd8206af4d4a
noneedsystem <noneedsystem@gmail.com>
2018-01-24 07:02:44 +0000

    singleHistory method added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cheerio   = require('cheerio');
 const URL = 'https://coinmarketcap.com';
 const SELECTOR_COINS    = 'table#currencies tbody tr';
 const SELECTOR_MARKETS  = '#markets-table tbody tr';
+const SELECTOR_HISTORY  = 'div#historical-data table tbody tr'
 const SELECTOR_EXCHANGE = 'div#markets table tbody tr';
 const SELECTOR_ALLCOINS = 'table#currencies-all tbody tr';
 
@@ -28,6 +29,12 @@ module.exports.single = (coinLink) =>
     .then(html => cheerio.load(html))
     .then($ => scrapSingle($))
     
+module.exports.singleHistory = (coinLink) =>
+  fetch(URL + coinLink + '/historical-data/?start=20130428&end=' + getHistoryEndDate())
+    .then(res => res.text())
+    .then(html => cheerio.load(html))
+    .then($ => scrapSingle($, true))
+    
 module.exports.exchange = (exchangeLink) =>
   fetch(URL + exchangeLink)
     .then(res => res.text())
@@ -41,12 +48,12 @@ module.exports.exchange = (exchangeLink) =>
         .then($ => scrapSingle($, coin))
     })))
     
-module.exports.all = () => 
+module.exports.all = (slc = 0) => 
   fetch(URL + '/all/views/all/')
     .then(res => res.text())
     .then(html => cheerio.load(html))
     .then($ => $(SELECTOR_ALLCOINS).map((i, el) => scrapSingleFromAll($(el))).get())
-    .then(coins => coins.slice(0, 2))
+    .then(coins => coins.slice(slc))
     
     
   function scrapCoin(el){
@@ -77,26 +84,28 @@ module.exports.all = () =>
   
   function scrapSingleFromAll(el){
     const elems = {
-      link: el.find('a.currency-name-container'),
-      supply: el.find('td.circulating-supply a'),
+      link:   el.find('a.currency-name-container'),
+      supply: el.find('td.circulating-supply').children().first(),
       symbol: el.find('td.col-symbol'),
     };
     
     return {
-      name: elems.link.text(),
-      supply: parseInt(elems.supply.attr('data-supply')),
-      symbol: elems.symbol.text()
+      name:   elems.link.text(),
+      supply: parseInt(Number(elems.supply.attr('data-supply'))),
+      symbol: elems.symbol.text(),
+      link:   elems.link.attr('href')
     }
   }
   
-  function scrapSingle($){
+  function scrapSingle($, history){
     const elems = {
       symbol: $('h1.text-large small.bold.hidden-xs'),
       // link:   $('a.currency-name-container'),
       rank:   $('span.glyphicon.glyphicon-star').next(),
       price:  $('span#quote_price'),
       cap:    $('span[data-currency-market-cap]'),
-      logo:   $('img.currency-logo-32x32')
+      logo:   $('img.currency-logo-32x32'),
+      web:    $('span.glyphicon.glyphicon-link').next(),
     };
     
     return scrapExtras($, {
@@ -112,8 +121,36 @@ module.exports.all = () =>
       price: {
         usd: elems.price.attr('data-usd'),
         //btc: elems.price.attr('data-btc')
-      }
-    })
+      },
+      web: elems.web && elems.web.attr('href')
+    }, history)
+  }
+  
+  function scrapHistory(el){
+    el = el.find('td');
+    const elems = {
+      date: el.eq(0),
+      open: el.eq(1),
+      high: el.eq(2),
+      low:  el.eq(3),
+      close:  el.eq(4),
+      volume: el.eq(5),
+      cap:    el.eq(6)
+    }
+    
+    return {
+      date: new Date(elems.date.text()).getTime(),
+      price: {
+        usd: {
+          open: Number(elems.open.text()),
+          high: Number(elems.high.text()),
+          low:  Number(elems.low.text()),
+          close: Number(elems.close.text()),
+        }
+      },
+      volume: elems.volume.attr('data-format-value'),
+      cap:    elems.cap.attr('data-format-value')
+    }
   }
   
   function scrapExchange(el){
@@ -132,14 +169,11 @@ module.exports.all = () =>
     }
   }
   
-  function scrapExtras($, coin){
-    const elems = {
-      logo: $('img.currency-logo-32x32'),
-      web:  $('span.glyphicon.glyphicon-link').next()
-    }
-    coin.logo = elems.logo.attr('src');
-    coin.web  = elems.web && elems.web.attr('href');
-    coin.markets = $(SELECTOR_MARKETS).map((i, el) => scrapMarket($(el))).get();
+  function scrapExtras($, coin, history = false){
+    if(!history)
+      coin.markets = $(SELECTOR_MARKETS).map((i, el) => scrapMarket($(el))).get();
+    else
+      coin.history = $(SELECTOR_HISTORY).map((i, el) => scrapHistory($(el))).get();
     return coin;
   }
   
@@ -168,4 +202,23 @@ module.exports.all = () =>
         btc: elems.price.attr('data-btc')
       }
     }
+  }
+  
+  
+  function getHistoryEndDate(){
+    const currentDate = new Date();
+    const arr = [ currentDate.getFullYear() ];
+    
+    let month = currentDate.getMonth() + 1;
+    if(month.length < 2)
+      month = '0' + month;
+    arr.push(month);
+    
+    let day = currentDate.getDate();
+    if(day.length < 2)
+      day = '0' + day;
+    arr.push(day);
+    
+    
+    return arr.join('');
   }
